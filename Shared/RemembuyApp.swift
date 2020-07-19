ba8cd86b8bb6bee89b5bd4bec4d41f7942531065
Anders Hovmöller <boxed@killingar.net>
2020-07-19 15:10:32 +0200

    Initial Commit

---
+++ Shared/RemembuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemembuyApp.swift
+//  Shared
+//
+//  Created by Anders Hovmöller on 2020-07-19.
+//
+
+import SwiftUI
+
+@main
+struct RemembuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
